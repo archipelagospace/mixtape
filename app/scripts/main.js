0104ae59906f203dde71ad11ebce3a5059c89446
João Doria <dev@joaodoria.com>
2016-06-12 12:16:14 +0200

    turns out js player not what I'm looking for
    stick to the api and it'll be alright

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,26 +6,19 @@ SC.initialize({
     redirect_uri: 'http://example.com/callback' // need to create this: http://mixtape.archipelago.space/auth.html
   });
 
-SC.get('/tracks').then(function(tracks){
-  alert('Latest track: ' + tracks[0].title);
+// SC.get('/users/233081566/tracks').then(function(tracks){
+//   alert('Latest track: ' + tracks[0].title);
+// });
+
+var track_url = 'http://soundcloud.com/archipelagospace/api-test';
+SC.oEmbed(track_url, { auto_play: true }).then(function(oEmbed) {
+  console.log('oEmbed response: ', oEmbed);
 });
 
-$(document).ready(
-   // This is the function that will get executed after the DOM is fully loaded 
-  function () {
-     // set user we wish to see trackList for 
-    var USER = '233081566'
-    /* set user name on DOM */
-    $('#user').html(USER);
-    
-     // Use soundcloud API to get track list 
-    SC.get('/users/'+USER+'/tracks', {limit: 100}, function(tracks){
-      var trackList = ''
-      for (var i = 0; i < tracks.length; i++) {
-         // populate trackList html 
-        trackList += tracks[i].title + '<br>';
-      }
-      $('#status').html(trackList);
-    });
-  }
-);
+//sc javascript player test
+      // $.scPlayer.defaults.onDomReady = function() {
+      //   $('a.sc-player, div.sc-player').scPlayer({
+      //     // randomize tracks in a playlist
+      //     randomize: true
+      //   });
+      // };
