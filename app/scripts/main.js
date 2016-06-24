640774c06c656a891ef2a2df5421c68e8d93b6ac
João Doria <dev@joaodoria.com>
2016-06-24 18:18:13 +0200

    layout in place

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
 console.log('The Archipelago Mixtape: A Tool for Journeys');
+
+        $(document).ready(function() {
+        $.simpleWeather({
+            location: 'Bergen, NO',
+            woeid: '',
+            unit: 'c',
+            success: function(weather) {
+              var html = '<p>'+'It is '+weather.currently+'</p>';
+
+              $('#weather').html(html);
+            },
+            error: function(error) {
+              $('#weather').html('<p>' + error + '</p>');
+            }
+          });
+        });
