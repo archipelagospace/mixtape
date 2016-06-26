022445fc347544f71a16fbb2b1d31a3b480a2b9a
João Doria <dev@joaodoria.com>
2016-06-26 15:05:56 +0200

    adding slidetoggles

--- app/scripts/playlist.js
+++ app/scripts/playlist.js
@@ -6,13 +6,13 @@ var playlist = function(e) {
     // initialisation:
     var pCount = 0;
     var playlistUrls = [
-        "../extras/01_Lunge_KristinAustreid.mp3",
-        "../extras/02_Windows_95_Audun_Mortensen.mp3",
-        "../extras/03_Untitled_(Tonlei)_Jason-Hendrik_Hansma.mp3",
-        "../extras/04_Mygg_Ingrid_Furre.mp3",
-        "../extras/05_Covering_of_the_hand_Glove_Femke_de_Vries.mp3",
-        "../extras/06_Hanging_Low_Rosalie_Schweiker_and_Jenny_Moore.mp3",
-        "../extras/07_5_erotic_stories_Inger_Wold_Lund.mp3"
+        '../extras/01_Lunge_KristinAustreid.mp3',
+        '../extras/02_Windows_95_Audun_Mortensen.mp3',
+        '../extras/03_Untitled_(Tonlei)_Jason-Hendrik_Hansma.mp3',
+        '../extras/04_Mygg_Ingrid_Furre.mp3',
+        '../extras/05_Covering_of_the_hand_Glove_Femke_de_Vries.mp3',
+        '../extras/06_Hanging_Low_Rosalie_Schweiker_and_Jenny_Moore.mp3',
+        '../extras/07_5_erotic_stories_Inger_Wold_Lund.mp3'
     ]; // audio list
     var loop = true;
 
@@ -33,19 +33,19 @@ var playing = false
 
 $(document).ready(function() {
     playlist();
-    $("#playbutton").click(function(){
+    $('#playbutton').click(function(){
       if (!playing) {
         howlerBank[0].play();
         playing = true;
       };
     });
-    $("#stopbutton").click(function() {
+    $('#stopbutton').click(function() {
       if (playing) {
         howlerBank[0].stop();
         playing = false
       };
     });
-    $("#pausebutton").click(function() {
+    $('#pausebutton').click(function() {
       if (playing) {
         howlerBank[0].pause();
           playing = false
